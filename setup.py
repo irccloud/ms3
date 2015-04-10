6b4e886c819a69abb247feb57de9e60e6ecb554b
Jonty Wareing <jonty@jonty.co.uk>
2015-04-10 17:35:16 +0100

    Take the setup version from the environment

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
+import os
  
 setup(
     name='ms3',
-    version='0.1.0',
+    version = os.environ.get('RELEASE_VERSION', '99.0.0.dev0'),
     author='Skoobe',
     packages=['ms3'],
     url='https://github.com/skoobe/ms3',
